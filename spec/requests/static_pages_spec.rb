61ee3e17e9b1deb771f257387efa23a52f9cc215
Andrew Elliott <contact@awegroup.com>
2012-06-15 15:22:45 -0700

    A full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe "StaticPages" do
+	let(:standardtitle) { "Ruby on Rails Tutorial Sample App"}
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -8,11 +12,16 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
 
-    it "should have the right title" do
-  	visit '/static_pages/home'
-  	page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Home")
-	end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Help page" do
@@ -25,7 +34,7 @@ describe "StaticPages" do
     it "should have the right title" do
   	visit '/static_pages/help'
   	page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Help")
+                    :text => "#{standardtitle} | Help")
 	end
   end
 
@@ -39,7 +48,21 @@ describe "StaticPages" do
     it "should have the right title" do
   	visit '/static_pages/about'
   	page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About")
+                    :text => "#{standardtitle} | About")
+	end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+  	visit '/static_pages/contact'
+  	page.should have_selector('title',
+                    :text => "#{standardtitle} | Contact")
 	end
   end
 end
